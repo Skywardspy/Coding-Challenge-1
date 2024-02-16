e8c285a32b4719c4dd9f30ced447d898fc875d18
Skywardspi <andregssampaio2@gmail.com>
2024-02-16 15:50:59 +0000

    Primeiro commit com o esqueleto básico das Operações e do Programa

---
+++ Operations.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Challenge_1
+{
+    internal class Operations
+    {
+        /// <summary>
+        /// Função que trata e processa os inputs
+        /// </summary>
+        /// <param name="inputs"> Operações enviadas pelo utilizador </param>
+        /// <returns></returns>
+        public static string Process(string[] inputs)
+        {
+            int[] resultQueue = new int[1];
+
+
+            return resultQueue.ToString();
+        }
+
+        /// <summary>
+        /// Operação PUSH - adiciona o valor recebido ao final da lista de resultados
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        /// <param name="value"> Valor a adicionar à lista </param>
+        private static void Push(int[] resultQueue, string value)
+        {
+            
+        }
+
+        /// <summary>
+        /// Operação ADD - soma os dois ultimos valores da lista 
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        private static void Add(int[] resultQueue)
+        {
+            
+        }
+
+        /// <summary>
+        /// Operação SUB - subtrai os dois ultimos valores da lista
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        private static void Subtract(int[] resultQueue)
+        {
+            
+        }
+
+        /// <summary>
+        /// Operação MUL - multiplica os dois ultimos valores da lista
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        private static void Multiply(int[] resultQueue)
+        {
+            
+        }
+
+        /// <summary>
+        /// Operação DIV - divide os dois ultimos valores da lista
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        private static void Divide(int[] resultQueue)
+        {
+            
+        }
+
+        /// <summary>
+        /// Operação DUP - duplica o ultimo valor da lista
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        private static void Duplicate(int[] resultQueue)
+        {
+            
+        }
+
+        /// <summary>
+        /// Operação POP - remove o ultimo valor da lista
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        private static void Pop(int[] resultQueue)
+        {
+            
+        }
+
+        /// <summary>
+        /// Operação SWAP - inverte a posição dos dois últimos valores da lista
+        /// </summary>
+        /// <param name="resultQueue"> Lista de resultados </param>
+        private static void Swap(int[] resultQueue)
+        {
+            
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Coding_Challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] treatedInput;
+
+            // Valida se o programa foi iniciado com ou sem parametros de entrada
+            if (args.Length == 0)
+            {
+                string? userInput;
+
+                Console.WriteLine("Insira a string de operações");
+                userInput = Console.ReadLine();
+
+                treatedInput = userInput.Split(' ');
+            } 
+            else
+            {
+                treatedInput = args;
+            }
+
+            // Processa as operações
+            Operations.Process(treatedInput);
+        }
+        
+    }
+
+}
+
